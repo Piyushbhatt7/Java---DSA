4b78e4f4e0b1f1135ee5e4c6ba3e2154ae22a48a
vengersmoon <vengersmon003@gmail.com>
2024-12-30 15:35:15 +0530

    t

--- src/Conditional.java
+++ src/Conditional.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 public class Conditional {
     public static void main(String[] args) {
         
-        System.out.print("Enter your age: ");
-        Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
+    //     System.out.print("Enter your age: ");
+    //     Scanner sc = new Scanner(System.in);
+    //     int age = sc.nextInt();
 
-        if(age > 18) {
-            System.out.println("Adult");
-        }
-        else{
-            System.out.println("Children");
-        }
-    }
+    //     if(age > 18) {
+    //         System.out.println("Adult");
+    //     }
+    //     else{
+    //         System.out.println("Children");
+    //     }
+    // }
+
+
+    // odd and even number check
     
 }
