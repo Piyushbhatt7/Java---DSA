904520a71fec057ccbdb74a6314f7f0712f816d2
vengersmoon <vengersmon003@gmail.com>
2024-12-30 15:39:57 +0530

    f

--- src/Conditional.java
+++ src/Conditional.java
@@ -24,7 +24,7 @@ public class Conditional {
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
     
-    if(num%2 == 0){
+    if(num % 2 == 0){
         System.out.println("Even number");
     }
     else {
