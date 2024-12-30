0c2326bd5982ed1d5ef822edfcad3cc6eff1783c
vengersmoon <vengersmon003@gmail.com>
2024-12-30 18:11:55 +0530

    f

--- src/Naturalnumber.java
+++ src/Naturalnumber.java
@@ -1,5 +1,13 @@
+
+import java.util.Scanner;
+
 public class Naturalnumber {
     public static void main(String[] args) {
+        
+        int i, n;
+
+        System.out.println("Enter any number: ");
+        Scanner sc = new Scanner(System.in);
         
     }
 }
