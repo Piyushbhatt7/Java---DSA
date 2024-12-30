93d258db4e48f05255094900a6eb9fa59a905f5e
vengersmoon <vengersmon003@gmail.com>
2024-12-30 17:47:48 +0530

    e

--- src/lops.java
+++ src/lops.java
@@ -1,7 +1,15 @@
 public class lops {
     public static void main(String[] args) {
-        for(int counter = 0; counter<10 ; counter++){
-            System.out.println(counter);
+        
+        
+        // for(int counter = 0; counter<10 ; counter++){
+        //     System.out.println(counter);
+        // }
+
+        int i = 0;
+        while(i< 10){
+            System.out.println(i);
+            i++;
         }
     }
 }
