938efe4899ad8b0877d1d878e534df828223fccc
vengersmoon <vengersmon003@gmail.com>
2024-12-30 17:11:31 +0530

    ff

--- src/Switch.java
+++ src/Switch.java
@@ -1,6 +1,20 @@
+
+import java.util.Scanner;
+
 public class Switch {
     public static void main(String[] args) {
         
-        
+        int button;
+
+        Scanner sc = new Scanner(System.in);
+        button = sc.nextInt();
+
+        if(button == 1){
+            System.out.println("Hello");
+        }
+
+        else if(button == 2){
+            System.out.println("Namaste");
+        }
     }
 }
