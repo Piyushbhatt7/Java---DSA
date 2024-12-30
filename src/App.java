95c66cf592eb9f023cd922b555c1fdbea9b9addd
vengersmoon <vengersmon003@gmail.com>
2024-12-30 14:46:14 +0530

    e

--- src/App.java
+++ src/App.java
@@ -3,9 +3,18 @@ public class App {
         // output 
         // System.out.println("Hello, World!");
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        int a = 10;
+        int b = 5;
+
+        int ans = a*b / a-b;
+        System.out.println(ans);
+
+        // int ans = (a*b) / (a-b);
+        // System.out.println(ans);
     }
 }
