ce8a16eb79a81fae2984c753bfcaae361ba2e30f
vengersmoon <vengersmon003@gmail.com>
2024-12-31 00:05:18 +0530

    g

--- src/SoliidRactangle.java
+++ src/SoliidRactangle.java
@@ -9,11 +9,14 @@ public class SoliidRactangle {
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+          // outer loop
 
-        for(i=1;i<n;i++){ 
-            for(j=1;j<n;j++){
+        for(i = 1; i <= n; i++){ 
+            // inner loop
+            for(j = 1; j <= n; j++){
                 System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
