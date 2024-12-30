09ee208295486f99e98b1bd6526596d77149601e
vengersmoon <vengersmon003@gmail.com>
2024-12-30 18:16:15 +0530

    j

--- src/Naturalnumber.java
+++ src/Naturalnumber.java
@@ -7,12 +7,13 @@ public class Naturalnumber {
         int i, n;
         int sum = 0;
 
-        System.out.println("Enter any number: ");
+        System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
         for(i = 1; i <= n ; i++){
               sum = sum+i;   // sum += i;
         }
+        System.out.println("Sum of natural number is = " + sum);
     }
 }
