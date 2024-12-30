663c0d1d03337ac448cc8449eab0a331d65130a9
vengersmoon <vengersmon003@gmail.com>
2024-12-30 18:13:59 +0530

    f

--- src/Naturalnumber.java
+++ src/Naturalnumber.java
@@ -5,13 +5,14 @@ public class Naturalnumber {
     public static void main(String[] args) {
         
         int i, n;
+        int sum = 0;
 
         System.out.println("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
         for(i = 1; i <= n ; i++){
-            
+              sum = sum+i;   // sum += i;
         }
     }
 }
