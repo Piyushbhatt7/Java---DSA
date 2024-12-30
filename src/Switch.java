4ff2b38eeed12800ccce4a0e37e4b2bae2f53492
vengersmoon <vengersmon003@gmail.com>
2024-12-30 17:24:15 +0530

    g

--- src/Switch.java
+++ src/Switch.java
@@ -1,29 +1,53 @@
 
 import java.util.Scanner;
 
+
+
 public class Switch {
     public static void main(String[] args) {
         
-        int button;
+        // int button;
         
+        // System.out.print("Enter number from 1-3: ");
+        // Scanner sc = new Scanner(System.in);
+        // button = sc.nextInt();
+
+        // if(button == 1){
+        //     System.out.println("Hello");
+        // }                                                       // By using if else statement
+
+        // else if(button == 2){
+        //     System.out.println("Namaste");
+        // }
+
+        // else if(button == 3){
+        //     System.out.println("Bonjour");
+        // }
+
+        // else{
+        //     System.out.println("Invalid Button");
+        // }
+
+        int button;
+
         System.out.print("Enter number from 1-3: ");
         Scanner sc = new Scanner(System.in);
         button = sc.nextInt();
 
-        if(button == 1){
-            System.out.println("Hello");
+        switch(button) {
+
+            case 1:
+                     System.out.println("Hello");
+                     break;
+
+            case 2:
+                     System.out.println("Namste");
+                     break;
+
+            case 3:  
+                    sout
         }
 
-        else if(button == 2){
-            System.out.println("Namaste");
-        }
 
-        else if(button == 3){
-            System.out.println("Bonjour");
-        }
-
-        else{
-            System.out.println("Invalid Button");
-        }
     }
 }
