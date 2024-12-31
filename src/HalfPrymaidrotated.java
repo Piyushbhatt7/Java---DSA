037e9c1c9bb5560567ca8dcaa0aab2c4a4c8077d
vengersmoon <vengersmon003@gmail.com>
2024-12-31 21:21:03 +0530

    ff

--- src/HalfPrymaidrotated.java
+++ src/HalfPrymaidrotated.java
@@ -6,15 +6,20 @@ public class HalfPrymaidrotated {
           
             int i,j,n;
 
-            System.out.println("Enter any number: ");
+            System.out.print("Enter any number: ");
             Scanner sc = new Scanner(System.in);
             n = sc.nextInt();
 
             for(i=1;i<=n;i++){
 
                   for(j=1;j<=n-i;j++){
+                        System.out.print(" ");
+                  }
+                 
+                  for(int k = 1;k<i;k++){
                         System.out.print("*");
                   }
+
                   System.out.println();
             }    
       }
