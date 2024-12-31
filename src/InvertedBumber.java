e0d477034f6a8feb7b1d9f40658be93df6ef2dcd
vengersmoon <vengersmon003@gmail.com>
2024-12-31 23:25:43 +0530

    f

--- src/InvertedBumber.java
+++ src/InvertedBumber.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class InvertedBumber {
     
     public static void main(String[] args) {
+        
+        int i,j,n;
+
+        System.out.println("Enter any number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
         
     }
 }
