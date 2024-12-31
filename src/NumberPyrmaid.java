676a8340a9bd803a3d118c944806df21f8a47706
vengersmoon <vengersmon003@gmail.com>
2024-12-31 21:30:20 +0530

    g

--- src/NumberPyrmaid.java
+++ src/NumberPyrmaid.java
@@ -1,6 +1,16 @@
+
+import java.util.Scanner;
+
 public class NumberPyrmaid {
     
     public static void main(String[] args) {
+        
+        int i,j,n;
+
+        System.out.println("Enter any number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
         
     }
 }
