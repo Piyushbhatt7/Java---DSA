5ab70341408af5b6a140dc929a217dd193aa6210
vengersmoon <vengersmon003@gmail.com>
2024-12-31 12:39:20 +0530

    ff

--- src/HollowRectangle.java
+++ src/HollowRectangle.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class HollowRectangle {
     public static void main(String[] args) {
          
-        int i,j,n;
+        int i= 4,j = 5, n;
         System.out.println("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-
+      
         
     }
 }
