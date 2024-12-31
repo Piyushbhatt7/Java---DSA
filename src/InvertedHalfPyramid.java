07001003312543d9cb1da70c621f3a0f13e2e021
vengersmoon <vengersmon003@gmail.com>
2024-12-31 13:02:40 +0530

    g

---
+++ src/InvertedHalfPyramid.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class InvertedHalfPyramid {
+    
+    public static void main(String[] args) {
+        
+        int i, j, n;
+        System.out.print("Enter any number: ");
+        Scanner sc = Scanner(System.in);
+        n = sc.nextInt();
+
+        
+    }
+}
