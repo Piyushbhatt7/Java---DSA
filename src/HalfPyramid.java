ba1f0e0b9f617df09b7485778407f45840921d4e
vengersmoon <vengersmon003@gmail.com>
2024-12-31 12:49:29 +0530

    f

--- src/HalfPyramid.java
+++ src/HalfPyramid.java
@@ -5,9 +5,12 @@ public class HalfPyramid {
     
     public static void main(String[] args) {
         
+        int i, j, n;
         System.out.print("Enter any number: ");
         Scanner sc = new  Scanner(System.in);
         n = sc.nextInt();
+
+        
 
     }
 }
