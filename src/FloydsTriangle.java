87bac40a4e6fd5efbecd2e91c5d3bfa737e21d04
vengersmoon <vengersmon003@gmail.com>
2025-01-01 13:47:35 +0530

    t

--- src/FloydsTriangle.java
+++ src/FloydsTriangle.java
@@ -7,11 +7,20 @@ public class FloydsTriangle {
     public static void main(String[] args) {
         
         int i,j,n;
+        int number = 1;
+
 
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n= sc.nextInt();
+       
+        for( i = 1; i<= n; i++){
 
-        
+            for(j = 1; j<=i; j++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+
     }
 }
