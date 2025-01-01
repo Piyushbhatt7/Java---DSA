a52e52951c72edd58f0561e004c74331ca4bf001
vengersmoon <vengersmon003@gmail.com>
2025-01-01 20:25:33 +0530

    h

---
+++ src/SolidRombus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SolidRombus {
+    
+    public static void main(String[] args) {
+        
+        int i,j,n;
+        System.out.print("Enter any number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+    }
+}
