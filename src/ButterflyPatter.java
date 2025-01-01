f956de5a31581ba9b3c75fdcf3d39aef5162e47c
vengersmoon <vengersmon003@gmail.com>
2025-01-01 20:06:44 +0530

    g

--- src/ButterflyPatter.java
+++ src/ButterflyPatter.java
@@ -1,7 +1,16 @@
+
+import java.util.Scanner;
+
 public class ButterflyPatter {
     
     public static void main(String[] args) {
         
+        int i,n,j;
+
+        System.out.println("Enter any number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
         
     }
 }
