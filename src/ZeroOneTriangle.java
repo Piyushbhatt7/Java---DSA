3f1c96dfa248e366a06b813d257307607a863622
vengersmoon <vengersmon003@gmail.com>
2025-01-01 13:50:43 +0530

    fg

---
+++ src/ZeroOneTriangle.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class ZeroOneTriangle {
+    
+    public static void main(String[] args) {
+        
+        int i,j,n;
+
+        System.out.println("Enter any number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+    }
+}
