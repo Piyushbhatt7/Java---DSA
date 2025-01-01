7e412b33955e7e3fbef155091a530236234ee547
vengersmoon <vengersmon003@gmail.com>
2025-01-01 22:19:00 +0530

    h

--- src/NumberPrymaid.java
+++ src/NumberPrymaid.java
@@ -7,10 +7,24 @@ public class NumberPrymaid {
         
         int i,n,j;
 
-        System.out.println("Enter any number: ");
+        System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
+        for(i=1;i<=n;i++){
+
+            for(j=1;j<=n-i;j++){
+
+                System.out.print(" ");
+
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+           
+        }
         
     }
 }
