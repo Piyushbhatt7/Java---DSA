2751b8f62036f1ace7076fb8de59ce4c7991d285
vengersmoon <vengersmon003@gmail.com>
2025-01-02 09:44:41 +0530

    g

--- src/PalindromicPattern.java
+++ src/PalindromicPattern.java
@@ -2,6 +2,8 @@ public class PalindromicPattern {
     
     public static void main(String[] args) {
         
-        
+        int i,j,n;
+
+        System.out.println("Enter any number: ");
     }
 }
