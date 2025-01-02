da75ab265c78c91ceae696cc640eddfefed1f97d
vengersmoon <vengersmon003@gmail.com>
2025-01-02 09:54:34 +0530

    g

--- src/PalindromicPattern.java
+++ src/PalindromicPattern.java
@@ -17,8 +17,8 @@ public class PalindromicPattern {
                 System.out.print(" ");
             }
 
-            for(j=2;j<=i;j++){
-                System.out.print(j-i);
+            for(j=i;j>=1;j--){
+                System.out.print(j);
             }
             System.out.println();
         }
