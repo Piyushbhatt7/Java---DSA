af94fb9bafccd5c8fe39d61855ef4cb2ecdde9aa
vengersmoon <vengersmon003@gmail.com>
2025-01-02 10:08:48 +0530

    g

--- src/DiamondPattern.java
+++ src/DiamondPattern.java
@@ -13,16 +13,17 @@ public class DiamondPattern {
 
         for(i=1;i<=n;i++){
 
-            for(j=1;j<i;j++){
+            for(j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
             for(j=n;j>=i;j--){
                 System.out.print("*");
             }
-            for(k=1;k<=i;k++){
-                System.out.println("*g");
+            for(k=1;k<i;k++){
+                System.out.print("*");
             }
             System.out.println();
         }
+        
     }
 }
