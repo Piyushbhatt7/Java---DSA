297a1aee0a35ae7ea0e7075dfa6bb9859b7cbea6
vengersmoon <vengersmon003@gmail.com>
2025-01-02 12:01:37 +0530

    f

---
+++ src/FunctionPractice.java
@@ -0,0 +1,11 @@
+public class FunctionPractice {
+    
+  public static int sumAdd(int num1, int num2){
+       return num1 + num2;
+  }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
