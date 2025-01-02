2d67a75911e0841514464d634d1d55312f5eabe1
vengersmoon <vengersmon003@gmail.com>
2025-01-02 12:04:07 +0530

    f

--- src/FunctionPractice.java
+++ src/FunctionPractice.java
@@ -1,11 +1,19 @@
+
+import java.util.Scanner;
+
 public class FunctionPractice {
     
   public static int sumAdd(int num1, int num2){
-       return num1 + num2;
+       return ;
   }
 
 
     public static void main(String[] args) {
-        
+          
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+       
     }
 }
