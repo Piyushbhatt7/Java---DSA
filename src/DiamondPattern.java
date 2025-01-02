e3674595973b5d6175b00ee2da0f10abd990b7d3
vengersmoon <vengersmon003@gmail.com>
2025-01-02 10:01:31 +0530

    e

--- src/DiamondPattern.java
+++ src/DiamondPattern.java
@@ -1,3 +1,10 @@
 public class DiamondPattern {
     
+    public static void main(String[] args) {
+        
+        int i,j,k,n;
+
+        System.out.print("Enter any number: ");
+        
+    }
 }
