194fe3628eff17eb20a3d57811c4d1dd2908b160
vengersmoon <vengersmon003@gmail.com>
2025-01-03 19:46:29 +0530

    f

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -2,7 +2,7 @@ public class ArrayPractice {
     
     public static void main(String[] args) {
         
-        System.out.println("Enter any number: ")
-        ;
+        System.out.println("Enter any number: ");
+        
     }
 }
