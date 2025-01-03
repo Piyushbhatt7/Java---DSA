a3baf1419f909c5a061995bc87c95e7fc8063ca7
vengersmoon <vengersmon003@gmail.com>
2025-01-03 19:49:28 +0530

    t

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -5,7 +5,7 @@ public class ArrayPractice {
     
     public static void main(String[] args) {
         
-        System.out.println("Enter any number: ");
+        System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
 
