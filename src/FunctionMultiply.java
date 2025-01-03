e667aa22cbc23b5ec7a7e37ff3eeffb8391861f8
vengersmoon <vengersmon003@gmail.com>
2025-01-03 11:03:27 +0530

    g

--- src/FunctionMultiply.java
+++ src/FunctionMultiply.java
@@ -1,7 +1,9 @@
 public class FunctionMultiply {
     
   public static int Mlti(int a, int b){
-    
+      
+    int sum = a*b;
+    return sum;
   }
 
     public static void main(String[] args) {
