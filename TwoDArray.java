13232b098b00bf359edf6d21fc375dd93d37f609
vengersmoon <vengersmon003@gmail.com>
2025-01-03 23:02:33 +0530

    g

--- TwoDArray.java
+++ TwoDArray.java
@@ -5,9 +5,10 @@ public class TwoDArray {
      
       public static void main(String[] args) {
           
-         System.out.print("Enter rows : ");
+         
          Scanner sc = new Scanner(System.in);
          int row = sc.nextInt();
+         System.out.print("Enter cols : ");
          int col = sc.nextInt();
 
          int [][] numbers = new int[row][col];
