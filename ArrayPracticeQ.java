b15fd9c36438aaa960c8139a6d58fc3cb2607907
vengersmoon <vengersmon003@gmail.com>
2025-01-03 20:11:38 +0530

    g

--- ArrayPracticeQ.java
+++ ArrayPracticeQ.java
@@ -10,6 +10,23 @@ public class ArrayPracticeQ {
         System.out.print("Enter the size of array: ");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
+        int number[] = new int [size];
+
+        for(int i=0;i<size;i++){
+            System.out.print("Enter numbers: ");
+             number[i] = sc.nextInt();
+        }
+        
+        System.out.print("Enter number to found!");
+        int x = sc.nextInt();
+
+        // output
+            
+        for(int i = 0 ; i<number.length; i++){
+             if(number[i] == x){
+                 System.out.println("X found at index " + i);
+             }
+        }
 
     }
 }
