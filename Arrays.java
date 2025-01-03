7a3b9f55d3ac03da3adac39656b9934759fcfaea
vengersmoon <vengersmon003@gmail.com>
2025-01-03 19:43:45 +0530

    m

--- Arrays.java
+++ Arrays.java
@@ -2,17 +2,19 @@ public class Arrays {
     
     public static void main(String[] args) {
         
-        int[] marks = new int[3]; // int marks[]
-        marks[0] = 97; //phy
-        marks[1] = 98; // chem
-        marks[2] = 98; // maths
+        // int[] marks = new int[3]; // int marks[]
+        // marks[0] = 97; //phy
+        // marks[1] = 98; // chem
+        // marks[2] = 98; // maths
 
-        // System.out.println(marks[0]);
-        // System.out.println(marks[1]);
-        // System.out.println(marks[2]);
+        // // System.out.println(marks[0]);
+        // // System.out.println(marks[1]);
+        // // System.out.println(marks[2]);
 
-        for(int i=0;i<3;i++){
-            System.out.println(marks[i]);  // for bigger numbers
-        }
+        // for(int i=0;i<3;i++){
+        //     System.out.println(marks[i]);  // for bigger numbers
+        // }
+
+        int marks[] = {97,98,89};
     }
 }
