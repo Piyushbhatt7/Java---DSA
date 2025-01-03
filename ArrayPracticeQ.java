67d306efdbf0cebf7a5e2c6882f65cf86d34db45
vengersmoon <vengersmon003@gmail.com>
2025-01-03 20:03:36 +0530

    b

---
+++ ArrayPracticeQ.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+// Take an array as input from user. Search for a given umber x and print the index at which it ocurs.
+
+
+public class ArrayPracticeQ {
+    public static void main(String[] args) {
+        
+        System.out.print("Enter the size of array: ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+    }
+}
