1b52108ffb5d6a0b3e0dd4b5fdc68044790b719a
vengersmoon <vengersmon003@gmail.com>
2025-01-03 19:35:09 +0530

    j

--- Arrays.java
+++ Arrays.java
@@ -2,7 +2,7 @@ public class Arrays {
     
     public static void main(String[] args) {
         
-        int[] marks = new int[3];
+        int[] marks = new int[3]; // int marks[]
         marks[0] = 97; //phy
         marks[1] = 98; // chem
         marks[2] = 98; // maths
