a3179c3c1a73f1eff7ce13a18366510eb5590891
vengersmoon <vengersmon003@gmail.com>
2025-01-03 19:27:58 +0530

    f

--- Arrays.java
+++ Arrays.java
@@ -7,8 +7,12 @@ public class Arrays {
         marks[1] = 98; // chem
         marks[2] = 98; // maths
 
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
     }
 }
