8858a15063f65eef3659488ecf4ff3735228f1c9
vengersmoon <vengersmon003@gmail.com>
2025-01-04 15:36:21 +0530

    nn

--- ArrayTraverse.java
+++ ArrayTraverse.java
@@ -12,7 +12,7 @@ public class ArrayTraverse {
 
         for(int i=0;i<size;i++){
             
-            System.out.print("Enter number: ");
+            System.out.print("Enter number: ");      // linear search [straigh fashion]
             number[i] = sc.nextInt();
         }
 
