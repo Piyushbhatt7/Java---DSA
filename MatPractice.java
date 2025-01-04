0b5de38fadc1e2f8511937e4132845f6a43ac20c
vengersmoon <vengersmon003@gmail.com>
2025-01-04 22:52:41 +0530

    t

--- MatPractice.java
+++ MatPractice.java
@@ -23,11 +23,6 @@ public class MatPractice {
 
         int x = sc.nextInt();
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-
-                System.out.println(numbers[i][j] + " ");
-            }
-        }
+        
     }
 }
