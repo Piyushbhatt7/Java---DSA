2dad482295359737ec94fe9ea1f5546ed2601c37
vengersmoon <vengersmon003@gmail.com>
2025-01-04 23:32:48 +0530

    ll

--- Strings.java
+++ Strings.java
@@ -1,9 +1,15 @@
+
+import java.util.Scanner;
+
 public class Strings {
     
     public static void main(String[] args) {
         
-        String name = "Kanchana";
-        String fullName = "Tony hulk";
-        String sentence = "HIII hulk and thor and all avengers";
+        // String name = "Kanchana";
+        // String fullName = "Tony hulk";
+        // String sentence = "HIII hulk and thor and all avengers";
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
     }
 }
