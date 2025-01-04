3d963f17f1b76f29070defa243f4ce42601e710b
vengersmoon <vengersmon003@gmail.com>
2025-01-04 15:19:48 +0530

    h

--- ArrayTraverse.java
+++ ArrayTraverse.java
@@ -5,18 +5,18 @@ public class ArrayTraverse {
     
     public static void main(String[] args) {
         
-        System.out.println("Enter the size of array: ");
+        System.out.print("Enter the size of array: ");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int number[] = new int[size];
 
         for(int i=0;i<size;i++){
             
-            System.out.println("Enter number: ");
+            System.out.print("Enter number: ");
             number[i] = sc.nextInt();
         }
 
-        System.out.println("Enter number to find : ");
+        System.out.print("Enter number to find : ");
         int x = sc.nextInt();
 
         // output
