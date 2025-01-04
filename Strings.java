93b649bdd2d9b6e2998ac86a789e51c33fad5759
vengersmoon <vengersmon003@gmail.com>
2025-01-04 23:31:38 +0530

    u

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    
+    public static void main(String[] args) {
+        
+        String name = "Kanchana";
+        String fullName = "Tony hulk";
+        String sentence = "HIII hulk and thor and all avengers";
+    }
+}
