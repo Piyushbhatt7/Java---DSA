32033e65b98353f00dc18323a7ea6944dd499d53
vengersmoon <vengersmon003@gmail.com>
2025-01-04 15:02:39 +0530

    g

--- MatPractice.java
+++ MatPractice.java
@@ -1,3 +1,5 @@
+// Take a matrix as input from user. Search for a given number x and print the idices at which it occur
+
 public class MatPractice {
     
     public static void main(String[] args) {
