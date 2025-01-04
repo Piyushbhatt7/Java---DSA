43adccd7630c327bcb11dc5e34c217cdc19d5288
vengersmoon <vengersmon003@gmail.com>
2025-01-04 15:15:12 +0530

    f

--- ArrayTraverse.java
+++ ArrayTraverse.java
@@ -1,7 +1,11 @@
+
+import java.util.Scanner;
+
 public class ArrayTraverse {
     
     public static void main(String[] args) {
         
         System.out.println("Enter the size of array: ");
+        Scanner sc = new Scanner(System.in);
     }
 }
