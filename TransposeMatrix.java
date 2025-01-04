8335709bb93157ec14c57e2ce8158f5a87988e00
vengersmoon <vengersmon003@gmail.com>
2025-01-04 23:05:15 +0530

    h

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -8,7 +8,11 @@ public class TransposeMatrix {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
         int cols = sc.nextInt();
+        int numbers[][] = new int[rows][cols];
 
-        
+        for(int i=0;i<rows;i++){
+            
+        }
+
     }
 }
