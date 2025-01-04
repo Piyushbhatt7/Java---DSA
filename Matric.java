e8b10086caa61c49915dacfd97e85b2458f8b87c
vengersmoon <vengersmon003@gmail.com>
2025-01-04 14:52:26 +0530

    g

---
+++ Matric.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Matric {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Enter number of rows : ");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        System.out.println("Enter number of columns : ");
+        
+    }
+}
