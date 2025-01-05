e02f7adea64ad9eeb1c9f6fcc5ef770f4ffd35df
vengersmoon <vengersmon003@gmail.com>
2025-01-05 22:58:55 +0530

    g

--- StringBuilders.java
+++ StringBuilders.java
@@ -2,6 +2,7 @@ public class StringBuilders {
     
     public static void main(String[] args) {
         
-        
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
     }
 }
