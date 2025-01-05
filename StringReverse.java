2a98c08e6280afadbbbd07e656b4555f4e6aa9cc
vengersmoon <vengersmon003@gmail.com>
2025-01-05 23:25:00 +0530

    t

--- StringReverse.java
+++ StringReverse.java
@@ -2,7 +2,7 @@ public class StringReverse {
     
     public static void main(String[] args) {
         
-        StringBuilder sb = new StringBuilder("Hello");
+        StringBuilder sb = new StringBuilder("Piyush");
 
         for(int i=0 ;i<sb.length()/2; i++){
 
