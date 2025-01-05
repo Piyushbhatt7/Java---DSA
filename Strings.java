7a294738a1073b2bfa441c773d03703f5230a7c2
vengersmoon <vengersmon003@gmail.com>
2025-01-05 22:38:41 +0530

    f

--- Strings.java
+++ Strings.java
@@ -64,7 +64,8 @@ public class Strings {
         //  }
 
         String sentence = "My name is Tony";
-        substring(beg index, end index);
+        
         String name = sentence.substring(11, sentence.length());
+        System.out.println(name);
     }
 }
