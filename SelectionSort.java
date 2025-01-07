4d97d2dfc3fe9add445ce5df272bf637f41dffb3
vengersmoon <vengersmon003@gmail.com>
2025-01-07 21:37:03 +0530

    u

--- SelectionSort.java
+++ SelectionSort.java
@@ -11,6 +11,10 @@ public class SelectionSort {
         
         int arr[] = {7,8,3,1,2};
 
-        for(int i=0; i<arr.length-1; i++)
+        for(int i=0; i<arr.length-1; i++){
+
+            int smallest = i;
+            
+        }
     }
 }
