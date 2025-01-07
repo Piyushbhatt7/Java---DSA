60f28980a7b7b0563808a85c561adcb8e162e1a5
vengersmoon <vengersmon003@gmail.com>
2025-01-07 21:42:05 +0530

    l

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         for(int i = 0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
-        System.out.println();
+        System.out.println(); 
     }
     public static void main(String[] args) {
         
@@ -30,3 +30,4 @@ public class SelectionSort {
         printArray(arr);
     }
 }
+ 
