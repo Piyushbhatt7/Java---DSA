8cc5b6aca54f86a2bbd11aaad2e4c2a93253b4ac
vengersmoon <vengersmon003@gmail.com>
2025-01-07 10:13:48 +0530

    h

--- Bits.java
+++ Bits.java
@@ -3,6 +3,17 @@ public class Bits {
     public static void main(String[] args) {
         
         int n = 5;
-        
+        int position = 2;
+
+        int bitMask = 1<<position;
+
+        if((bitMask & n) == 0) {
+
+            System.out.println("Bit was 0");
+        }
+
+        else{
+            System.out.println("Bit was 1");
+        }
     }
 }
