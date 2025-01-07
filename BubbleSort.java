3efdeba65938ebafb16715ea3c84512641630e1a
vengersmoon <vengersmon003@gmail.com>
2025-01-07 18:13:39 +0530

    y

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,8 +13,10 @@ public class BubbleSort {
 
         // bubble sort
 
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=0; j<arr.length-i-1; j++){
+        // Time complexity = 0(n^2)
+
+        for(int i=0; i<arr.length-1; i++){          // n-1
+            for(int j=0; j<arr.length-i-1; j++){    
                 if(arr[j] > arr[j+1]) {
 
                     // swap
