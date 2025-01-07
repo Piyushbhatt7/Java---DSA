ff41371a0b8f2b2337675178809bde612ae83c4d
vengersmoon <vengersmon003@gmail.com>
2025-01-07 10:18:44 +0530

    g

--- SetBit.java
+++ SetBit.java
@@ -2,6 +2,11 @@ public class SetBit {
     
     public static void main(String[] args) {
         
+        int n = 5;
+        int position = 1;
+
+        int bitMask = <<position;
+
         
     }
 }
