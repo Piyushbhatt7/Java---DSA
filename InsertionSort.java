ac34b2f85afb6ecaf179a8fd3f3ae523a14e1c3a
vengersmoon <vengersmon003@gmail.com>
2025-01-08 18:00:56 +0530

    y

--- InsertionSort.java
+++ InsertionSort.java
@@ -16,7 +16,11 @@ public class InsertionSort {
 
             int current = arr[i];
             int j = i-1;
-            while(j >=0 && )
+            while(j >=0 && arr[current] < arr[j]) {
+
+                arr[j+1] = arr[j];
+                j--;
+            }
         }
         
     }
