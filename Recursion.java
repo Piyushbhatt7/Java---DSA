86a8e45d93b06ed9ae69522c3211c03b4130fbf3
vengersmoon <vengersmon003@gmail.com>
2025-01-08 19:04:49 +0530

    j

--- Recursion.java
+++ Recursion.java
@@ -1,10 +1,13 @@
 public class Recursion {
     
-   
+    public static void printNum(int n){
+        System.out.println(n);
+        printNum(n-1);
+    }
 
     public static void main(String[] args) {
         
         int n = 5;
-        
+
     }
 }
