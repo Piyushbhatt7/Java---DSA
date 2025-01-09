6cdbbaaa3572fb5f21db492f3ef3257a5204036a
vengersmoon <vengersmon003@gmail.com>
2025-01-09 13:17:27 +0530

    j

--- NaturalNumber.java
+++ NaturalNumber.java
@@ -1,7 +1,11 @@
 public class NaturalNumber {
     
     public static void printSum (int i, int n, int sum) {
+        if(i == n) {
 
+            sum+= i;
+            System.out.println(sum);
+        }
         sum += i;
         printSum(i+1, n, sum);
     }
