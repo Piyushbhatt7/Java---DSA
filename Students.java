c116a605d04219ecbeec06fab7edc92f2d242742
vengersmoon <vengersmon003@gmail.com>
2025-01-10 12:00:39 +0530

    ljjhj

---
+++ Students.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class Students {
+    
+    
+   // class First {
+       
+    int rollno;
+    String name;
+
+      void printData(int r, String n) {
+         
+        rollno = r;
+        name = n;
+      }
+
+      void printInfo() {
+         
+        System.out.print("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        
+       
+        System.out.print("Enter you rollNo.: ");
+        rollno = sc.nextInt();
+      //}
+         
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+       
+        Students obj = new Students();
+        obj.printInfo();
+        System.out.println(obj.name );
+        System.out.println(obj.rollno);
+
+    }
+}
