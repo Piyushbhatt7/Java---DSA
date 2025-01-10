3945b032cbd52affbcd5afb96be48484dc8ffa81
vengersmoon <vengersmon003@gmail.com>
2025-01-10 12:48:37 +0530

    kk

---
+++ BoxDemo.java
@@ -0,0 +1,32 @@
+class Box {
+
+    double height, width, depth;
+
+    Box(double l) {
+
+        height=width=depth=l;
+
+    }
+
+    Box(double h, double w, double d) {
+
+        height = h;
+        width = w;
+        depth = d;
+    }
+
+    
+    
+}
+
+
+
+public class BoxDemo {
+    
+    public static void main(String[] args) {
+        
+        Box obj = new Box(12, 22, 78);
+        System.out.println("The product of height width and depth is = " + obj.height * obj.width * obj.depth);
+        System.out.println(obj.height + "\n" + obj.width + "\n" + obj.depth);
+    }
+}
