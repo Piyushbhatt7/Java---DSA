28efa51abb20e87a43c572bf2c64210219207e77
vengersmoon <vengersmon003@gmail.com>
2025-01-10 12:15:23 +0530

    kk

---
+++ Const.java
@@ -0,0 +1,35 @@
+public class Const {
+    
+ int age, id;
+ String name;
+
+  Const(int a, String n) {
+     
+       age = a;
+       name = n;
+  }
+
+  Const(int i) {
+     
+    id = i;
+  }
+
+  Const(int i, String n, int a) {
+     
+    age = a;
+    name = n;
+    id = i;
+  }
+
+  void display() {
+
+    System.out.println(name + " " + age + " " + id);
+  }
+
+
+    public static void main(String[] args) {
+        
+        Const obj = new Const(10623, "Piyush", 20);
+        obj.display();
+    }
+}
