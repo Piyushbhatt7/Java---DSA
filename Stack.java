9d7fc6114f79b8a5f25c55e0945cc103bd414a5d
vengersmoon <vengersmon003@gmail.com>
2025-01-11 14:52:02 +0530

    jj

---
+++ Stack.java
@@ -0,0 +1,29 @@
+
+class Stack {
+
+    int stack[] = new int[10];
+    int tos;
+
+     Stack()
+      {
+
+        tos = -1;
+    }
+    
+    void push(int item) {
+        if(tos==9)
+        {
+            
+        }
+    }
+    
+}
+
+
+
+public class Stack {
+    
+    public static void main(String[] args) {
+        
+    }
+}
