8514f6a271ba08890cec88535a816142a05c8fdb
vengersmoon <vengersmon003@gmail.com>
2025-01-11 14:48:25 +0530

    gg

--- PrePostIncrement.java
+++ PrePostIncrement.java
@@ -2,15 +2,15 @@ public class PrePostIncrement {
     
     public static void main(String[] args) {
         
-    //    int a = 10;
-    //    int b = 0;
+       int a = 10;
+       int b = 0;
 
     //    b = a--;
     // b = --a;
-    //    System.out.println(a);
-    //    System.out.print(b);
+       System.out.println(a);
+       System.out.print(b);
 
-    int a = 10, b = 20, c = 30;
+    // int a = 10, b = 20, c = 30;
 
     // if(a<b && b<c){
     //     System.out.println(true);
