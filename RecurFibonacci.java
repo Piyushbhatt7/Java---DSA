7287d6aab36122074131e8d851bdd334e61a222b
vengersmoon <vengersmon003@gmail.com>
2025-01-14 10:47:09 +0530

    ll

--- RecurFibonacci.java
+++ RecurFibonacci.java
@@ -2,7 +2,11 @@ public class RecurFibonacci {
     
    public static int printFibo(int n)
    {
-      
+       int a,b;
+       int fibo_c = a + b ;
+       a = b;
+       b = fibo_c;
+
    }
 
     public static void main(String[] args) {
