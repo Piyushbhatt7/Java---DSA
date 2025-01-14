1f317168561452707941b151c5255f2c986ec363
vengersmoon <vengersmon003@gmail.com>
2025-01-14 10:27:34 +0530

    ff

--- RecurFactorial.java
+++ RecurFactorial.java
@@ -1,9 +1,13 @@
 public class RecurFactorial {
     
 
-   public void printFactorial(int i, n)
-   {
-       
+   public static int printFactorial(int n)
+   {   if(n == 1) {
+         return 1;
+   }
+       int fact = printFactorial(n-1);
+       int fact_n = n*fact;
+       return  fact_n;
    }
 
 
