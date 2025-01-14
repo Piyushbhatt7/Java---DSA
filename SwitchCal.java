0a4b16c9a51d78bb05ddec758ce4e3921d11bddf
vengersmoon <vengersmon003@gmail.com>
2025-01-14 15:19:04 +0530

    g

--- SwitchCal.java
+++ SwitchCal.java
@@ -15,6 +15,26 @@ public class SwitchCal {
         System.out.print("Choose any one operator ('+,-,*,/,%'): ");
         operat = sc.next().charAt(0);
 
-        
+        switch (operat) {
+            case '+':
+                    sum = num1 + num2;
+                    break;
+            case '-':
+                    sum = num1 - num2;
+                    break;
+            case '*':
+                    sum = num1 * num2;
+                    break;
+            case '/':
+                    sum = num1 / num2;
+                    break;
+            case '%':
+                    sum = num1 / num2;
+
+            default:
+                throw new AssertionError("Invalid input");
+        }
+
+        System.out.println(sum);
     }
 }
