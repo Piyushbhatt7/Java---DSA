22c95665678e49321c8fb82949c5cc715aee78a8
vengersmoon <vengersmon003@gmail.com>
2025-01-14 15:26:02 +0530

    l

---
+++ RecurAdd.java
@@ -0,0 +1,18 @@
+public class RecurAdd {
+    
+    public static int printAdd(int n)
+    {
+        if(n == 1)
+        {
+            return;
+        }
+            System.out.println();
+    }
+
+
+
+    public static void main(String[] args) {
+        
+
+    }
+}
