c05bef9281e28dcbf9f761a8469b040d3858ea23
vengersmoon <vengersmon003@gmail.com>
2025-01-14 10:53:14 +0530

    gg

--- RecurFibonacci.java
+++ RecurFibonacci.java
@@ -13,5 +13,8 @@ public class RecurFibonacci {
 
     public static void main(String[] args) {
         
+        int a=0, b=1;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
