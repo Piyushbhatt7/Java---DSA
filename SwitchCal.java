47d76c4d273ffc1eb58d72cdddec8f55a53f3ebf
vengersmoon <vengersmon003@gmail.com>
2025-01-14 15:14:31 +0530

    f

--- SwitchCal.java
+++ SwitchCal.java
@@ -1,6 +1,17 @@
+
+import java.util.Scanner;
+
 public class SwitchCal {
     
     public static void main(String[] args) {
+        
+        double num1, num2, sum;
+        char operat;
+        System.out.print("Enter number1: ");
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextDouble();
+        System.out.println("Enter number2: ");
+        num2 = sc.nextDouble();
         
     }
 }
