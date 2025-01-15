5e59f8f36a519e793b96954a532539a2588a6f9a
vengersmoon <vengersmon003@gmail.com>
2025-01-15 19:43:24 +0530

    hh

--- RecurLogN.java
+++ RecurLogN.java
@@ -1,7 +1,29 @@
 public class RecurLogN {
     
+    public static int calculatePower(int x, int n)
+    {   
+     
+ 
+     if (n == 0) {
+ 
+         return 1;
+     }
+ 
+ 
+     if(x == 0) 
+     {
+         return 0;
+     }
+        int xPower = calculatePower(x, n-1);
+        int xPow = x * xPower;
+        return xPow;
+    }
+
+
+
+
     public static void main(String[] args) {
         
-        
+
     }
 }
