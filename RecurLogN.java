98ecbbb7e67a3834877cd7f09501c59eb05f4473
vengersmoon <vengersmon003@gmail.com>
2025-01-15 19:45:50 +0530

    g

--- RecurLogN.java
+++ RecurLogN.java
@@ -23,7 +23,7 @@ public class RecurLogN {
        }
        else{
 
-        return calculatePower(x, n/2) * calculatePower(x, n/2)
+        return calculatePower(x, n/2) * calculatePower(x, n/2) * x;
        }
     }
 
