fb4006685124c760cf7ae6989ad9e76b9c033302
vengersmoon <vengersmon003@gmail.com>
2025-01-16 13:28:35 +0530

    gg

--- TowerofHanoi.java
+++ TowerofHanoi.java
@@ -1,6 +1,6 @@
 public class TowerofHanoi {
     
-    public static void TowerofHanoi(int n, string)
+    public static void TowerofHanoi(int n, String src)
     
 
     public static void main(String[] args) {
