d59aae0ef0cb6d054c7ad44947be07f5a8b8c47f
vengersmoon <vengersmon003@gmail.com>
2025-01-16 13:31:52 +0530

    g

--- TowerofHanoi.java
+++ TowerofHanoi.java
@@ -3,7 +3,7 @@ public class TowerofHanoi {
     public static void TowerofHanoi(int n, String src, String helper, String dest)
     {
         TowerofHanoi(n-1, src, dest, helper);
-        
+        System.out.println("transfer disk " + n + " from " + src + " to " + dest);
     }
     
 
