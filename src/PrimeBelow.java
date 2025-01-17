53a6822a4497cdaf490b5c7e9cce17e769b9165b
vengersmoon <vengersmon003@gmail.com>
2025-01-17 12:25:16 +0530

    l

--- src/PrimeBelow.java
+++ src/PrimeBelow.java
@@ -3,7 +3,7 @@ public class PrimeBelow {
     public static void main(String[] args) {
         
 
-        for(int i=2; i<=100; i++)
+        for(int i=2; i<=100;)
         {
             int primev = 1;
 
@@ -21,7 +21,7 @@ public class PrimeBelow {
                 i++;
             }
             else {
-                
+                i++;
             }
         }
     }
