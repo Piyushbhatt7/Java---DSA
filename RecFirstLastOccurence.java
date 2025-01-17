101d6d79c010c41fa76b6dda7e48eefd8faae939
vengersmoon <vengersmon003@gmail.com>
2025-01-17 11:07:34 +0530

    g

--- RecFirstLastOccurence.java
+++ RecFirstLastOccurence.java
@@ -6,6 +6,10 @@ public class RecFirstLastOccurence {
    public static void findOccurance(String str, int idx, char element)
    {
      char currChar = str.charAt(idx);
+     if(currChar == element)
+     {
+        
+     }
    }
 
 
