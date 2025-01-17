5d75e6db64d0487308c7b68870101a59fbd69850
vengersmoon <vengersmon003@gmail.com>
2025-01-17 10:55:03 +0530

    f

--- RecuerStringReverse.java
+++ RecuerStringReverse.java
@@ -3,6 +3,7 @@ public class RecuerStringReverse {
     public static void printReverse(String str, int ind)
     {
         System.out.println(str.charAt(ind));
+        printReverse(str, ind-1);
     }
 
     public static void main(String[] args) {
