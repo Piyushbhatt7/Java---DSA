46e3fe4c00218998662e202407f275ade104eb94
vengersmoon <vengersmon003@gmail.com>
2025-01-17 11:06:00 +0530

    f

--- RecFirstLastOccurence.java
+++ RecFirstLastOccurence.java
@@ -1,7 +1,7 @@
 public class RecFirstLastOccurence {
     
    public static int first = -1;
-   public static int last = -1
+   public static int last = -1;
 
 
     public static void main(String[] args) {
