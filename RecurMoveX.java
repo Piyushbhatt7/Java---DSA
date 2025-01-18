ef2d16ccb9cfc9e6d63a914de168ac29a40d25cf
vengersmoon <vengersmon003@gmail.com>
2025-01-18 23:24:38 +0530

    j

--- RecurMoveX.java
+++ RecurMoveX.java
@@ -14,6 +14,7 @@ public class RecurMoveX {
         else{
 
             newString += currChar;// newstring = new string + currchar
+            moveAllX(str, idx+1, count, newString);
         }
     }
 
