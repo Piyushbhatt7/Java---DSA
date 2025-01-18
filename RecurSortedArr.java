ec4e6a5b253b40a55b2f7f448a2d312254510932
vengersmoon <vengersmon003@gmail.com>
2025-01-18 23:14:42 +0530

    t

--- RecurSortedArr.java
+++ RecurSortedArr.java
@@ -21,7 +21,7 @@ public class RecurSortedArr {
 
     public static void main(String[] args) {
         
-        int arr[] = {1,3,5};
+        int arr[] = {1,3,4};
         System.out.println(isSoerted(arr, 0));
 
     }
