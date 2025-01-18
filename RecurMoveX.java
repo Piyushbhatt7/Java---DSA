da824a4feb8b81971b898d098a2fc0b5f296f7e1
vengersmoon <vengersmon003@gmail.com>
2025-01-18 23:21:56 +0530

    k

--- RecurMoveX.java
+++ RecurMoveX.java
@@ -2,7 +2,8 @@ public class RecurMoveX {
     
 
     // Moving all x in the end "axbcxxd"
-
+     
+    public static  void moveAllX(String str, int idx)
 
     public static void main(String[] args) {
         
