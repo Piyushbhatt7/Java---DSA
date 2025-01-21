b2646ee795f442ef93b5ec8a04d76a41bbb5a8b8
vengersmoon <vengersmon003@gmail.com>
2025-01-21 15:20:31 +0530

    gg

--- PractiseRecurKeyboarfdCombination.java
+++ PractiseRecurKeyboarfdCombination.java
@@ -1,10 +1,12 @@
 public class PractiseRecurKeyboarfdCombination {
     
+    public static String[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"};
 
+    
 
 
 
     public static void main(String[] args) {
-        s
+        
     }
 }
