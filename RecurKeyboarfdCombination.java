a3c53cb9e4434ad35b433a19056da706089e8908
vengersmoon <vengersmon003@gmail.com>
2025-01-21 10:54:06 +0530

    r

--- RecurKeyboarfdCombination.java
+++ RecurKeyboarfdCombination.java
@@ -3,7 +3,9 @@ public class RecurKeyboarfdCombination {
    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"};
 
    public static void printCombinattions(String str, int idx, String combination) {
-    
+      
+       char currchar = str.charAt(idx);
+       String mapping = keypad[currchar - '0'];
    }
 
 
