f66bf8468aa5f9996581cccb63e948e649f416e2
vengersmoon <vengersmon003@gmail.com>
2025-01-21 10:52:02 +0530

    c

--- RecurKeyboarfdCombination.java
+++ RecurKeyboarfdCombination.java
@@ -1,11 +1,11 @@
 public class RecurKeyboarfdCombination {
     
-
+   public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz}
 
 
 
     public static void main(String args[])
     {
-        
+
     }
 }
