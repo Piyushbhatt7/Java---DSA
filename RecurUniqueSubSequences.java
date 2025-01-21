29b64da7c21d6b16d2c78249a7e1eae8210e0ca7
vengersmoon <vengersmon003@gmail.com>
2025-01-21 10:35:46 +0530

    r

--- RecurUniqueSubSequences.java
+++ RecurUniqueSubSequences.java
@@ -2,6 +2,10 @@ public class RecurUniqueSubSequences {
    
     public static void UniqueSequence(String str, int idx, String newString) {
         
+        if( idx == str.length())
+        {
+            System.out.println(newString);
+        }
 
         char currChar = str.charAt(idx);
         UniqueSequence(str, idx+1, newString + currChar);
