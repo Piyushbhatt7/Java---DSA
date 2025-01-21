8e95ddd6c1ce142552da19eb4da5fc169657a295
vengersmoon <vengersmon003@gmail.com>
2025-01-21 10:18:58 +0530

    g

--- RecurSequence.java
+++ RecurSequence.java
@@ -6,7 +6,7 @@ public static void subsequences(String str, int idx, String newString) {
 
     char currChar = str.charAt(idx);
     // to be
-
+    subsequences(str, idx+1, newString+currChar);
 
     // or not to be
 }
