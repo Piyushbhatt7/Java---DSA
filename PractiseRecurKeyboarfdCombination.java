3ae317a23024176840ae977f58367e5ffc9c9245
vengersmoon <vengersmon003@gmail.com>
2025-01-21 15:22:48 +0530

    j

--- PractiseRecurKeyboarfdCombination.java
+++ PractiseRecurKeyboarfdCombination.java
@@ -5,6 +5,9 @@ public class PractiseRecurKeyboarfdCombination {
     public static void printCombination(String str, int idx, String combination)
     {
         char currChar = str.charAt(idx);
+        String mapping = keypad[currChar - '0'];
+
+        
     }
 
 
