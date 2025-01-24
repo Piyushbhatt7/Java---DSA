8bc24148a6477d2093d1cc376a50328ed0932a13
vengersmoon <vengersmon003@gmail.com>
2025-01-24 11:38:00 +0530

    f

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -4,12 +4,22 @@ class Test {
     {
         System.out.println("No parameters");
     }
+
+    void Demo(int a)
+    {
+        System.out.println("a: " + a);
+    }
+
+    void Demo(int a, int b)
+    {
+        System.out.println("a: " + a + "b: " + b);
+    }
+
+    void Demo(double a)
+    {
+        System.out.println("a" + a);
+    }
 }
-
-
-
-
-
 
 
 public class MethodOverloading {
