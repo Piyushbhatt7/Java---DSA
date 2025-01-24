ae0607bf27a4ec2a6c8c6b138039f5962cc8c5db
vengersmoon <vengersmon003@gmail.com>
2025-01-24 10:52:13 +0530

    default constructor

---
+++ DefaultConstructor.java
@@ -0,0 +1,17 @@
+class Counst
+{
+    Counst()
+    {
+        System.out.println("Deafault constructor!");
+    }
+}
+
+
+public class DefaultConstructor {
+    
+
+    public static void main(String[] args) {
+        
+        Counst obj = new Counst();
+    }
+}
