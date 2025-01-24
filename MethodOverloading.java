930d736d8e6ab806c5cc7710dfd60ccec6ed8ee5
vengersmoon <vengersmon003@gmail.com>
2025-01-24 11:44:42 +0530

    dd

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -15,13 +15,27 @@ class Test {
         System.out.println("a: " + a + "b: " + b);
     }
 
-    void Demo(double a)
+    double Demo(double a)
     {
-        System.out.println("a" + a);
+        System.out.println("a " + a);
+        return a*a;
     }
 }
 
 
 public class MethodOverloading {
     
+    public static void main(String[] args) {
+        
+        Test obj = new Test();
+        obj.Demo();
+        obj.Demo(20);
+        obj.Demo(10, 30);
+        double result = obj.Demo(53.45);
+
+        System.out.println(obj.Demo(53.45) + result);
+    }
+    
+    
+
 }
