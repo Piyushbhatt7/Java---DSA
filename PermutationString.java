40f3e30ec904f8f6c1fae4002fb0bf47034d2856
vengersmoon <vengersmon003@gmail.com>
2025-01-25 13:19:20 +0530

    t

--- PermutationString.java
+++ PermutationString.java
@@ -2,7 +2,7 @@ public class PermutationString {
     
     public static void printPermutation(String str, String permu)
     {
-        
+        for(int i=0; i<str.length(); i++)
     }
     
     
