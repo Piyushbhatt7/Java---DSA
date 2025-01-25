fcfa7911991a6051d3b115b76990b8f0322650a1
vengersmoon <vengersmon003@gmail.com>
2025-01-25 23:56:15 +0530

    y

--- PermutationString.java
+++ PermutationString.java
@@ -8,7 +8,7 @@ public class PermutationString {
             System.out.println(permu);
             return;
         }
-g rggbg
+g rggbgh
 vf tt rryytttr
         for(int i=0; i<str.length(); i++)
         {
