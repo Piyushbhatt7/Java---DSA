4b950ba8ea55e03793a23739a6c7c3d29d3783a0
vengersmoon <vengersmon003@gmail.com>
2025-01-25 10:55:48 +0530

    t

--- ConstructorOverloading.java
+++ ConstructorOverloading.java
@@ -1,5 +1,15 @@
-class ConstructorOverloading{
+class Constructor{
     
+    Constructor()
+    {
+        System.out.println("hii");
+    }
+
+    Constructor(String a)
+    {
+        String name  = a;
+        System.out.println(name);
+    }
 }
 
 public class ConstructorOverloading {
