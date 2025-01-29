4a9094f2d5b97119509aa46147c8d53680f0a923
vengersmoon <vengersmon003@gmail.com>
2025-01-29 19:40:55 +0530

    g

--- MazeMove.java
+++ MazeMove.java
@@ -25,6 +25,7 @@ public class MazeMove {
         
         int n=3, m = 3;
 
-        int totalpaths = countPaths(0, 0, n, m)
+        int totalpaths = countPaths(0, 0, n, m);
+        System.out.println(tota);
     }
 }
