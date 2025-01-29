973d3e5424d5d6f70ee83eb8d157681579da8446
vengersmoon <vengersmon003@gmail.com>
2025-01-29 20:05:44 +0530

    g

--- PlaceTile.java
+++ PlaceTile.java
@@ -1,10 +1,14 @@
 public class PlaceTile {
    
     
+  public static int placeTiles(String int n, int m)
+  {
+    // vertically 
+    int verticalPlacment
+  }
 
 
 
-    
     public static void main(String[] args) {
         
     }
