8c0e441281a631c3ba04f1da835b94d36e941c5a
vengersmoon <vengersmon003@gmail.com>
2025-01-29 19:38:04 +0530

    t

--- MazeMove.java
+++ MazeMove.java
@@ -1,6 +1,20 @@
 public class MazeMove {
+
+     
+         int countPaths(int i, int j, int n, int m)
+         {
+            
+            // downwards
+            int downpath = countPaths(i+1, j, n, m);
+
+            int rightpath = countPaths(i, j+1, n, m);
+
+            return downpath + rightpath;
+         }
+    
     
     public static void main(String[] args) {
         
+        int n=3, m = 3;
     }
 }
