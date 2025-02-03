d719f1baad0c72d8a37e9b7e4561886e09e0e995
vengersmoon <vengersmon003@gmail.com>
2025-02-03 13:53:51 +0530

    f

---
+++ Constructorloading.java
@@ -0,0 +1,23 @@
+class Haseena {
+
+    int a, b,c;
+
+    Haseena(int num1, int num2)
+    {
+        num1 = a;
+        num2 = b;
+
+        c = num1 + num2;
+        System.out.println(c);
+    }
+}
+
+
+
+public class Constructorloading {
+    
+    public static void main(String args[])
+    {
+
+    }
+}
