d0964faaeba3b453152b0c2d4564bb2ceb1bac74
vengersmoon <vengersmon003@gmail.com>
2025-02-03 22:23:49 +0530

    g

--- Classes.java
+++ Classes.java
@@ -2,7 +2,7 @@ class A
 {
     void myclass()
     {
-        int a,b;
+        int a = 5, b = 90;
         int c = a + b;
         System.out.println(c);
     }
@@ -13,5 +13,7 @@ public class Classes {
     
     public static void main(String[] args) {
         
+        A obj = new A();
+        obj.myclass();
     }
 }
