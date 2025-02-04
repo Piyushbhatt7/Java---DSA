59960a4541c07c56c48448a480bb923351a533a0
vengersmoon <vengersmon003@gmail.com>
2025-02-04 13:34:06 +0530

    f

--- Recursion3.java
+++ Recursion3.java
@@ -2,12 +2,16 @@ public class Recursion3 {
 
     public static int callGuests(int n)
     {
+
+        
         // single
         int ways1 = callGuests(n-1);
 
         // duble
 
-        int ways2 = (n-1) * callGuests(n-2)
+        int ways2 = (n-1) * callGuests(n-2);
+
+        return ways1 + ways2;
 
     }
     
