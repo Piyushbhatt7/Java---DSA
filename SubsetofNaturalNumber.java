c3c639c62a5f7e1ba35b6fef678d21a5c5e19f81
vengersmoon <vengersmon003@gmail.com>
2025-02-04 13:39:35 +0530

    t

--- SubsetofNaturalNumber.java
+++ SubsetofNaturalNumber.java
@@ -1,8 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SubsetofNaturalNumber {
 
     public static 
     
     public static void main(String[] args) {
         
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
     }
 }
