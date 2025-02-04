1f908d9679ab0e5d3b7b0fedf63cc6f391cfaf39
vengersmoon <vengersmon003@gmail.com>
2025-02-04 13:44:49 +0530

    g

--- SubsetofNaturalNumber.java
+++ SubsetofNaturalNumber.java
@@ -2,12 +2,21 @@ import java.util.ArrayList;
 
 public class SubsetofNaturalNumber {
 
+    public static void printSubset(ArrayList<Integer> subset)
+    {
+        for(int 1 = 0; i<subset.size(); i++)
+        {
+            System.out.println();
+        }
+    }
+
     public static void findSubsets(int n, ArrayList<Integer> subset)
     {
 
         if(n == 0)
         {
             printSubset(subset);
+            return;
         }
 
         // add
