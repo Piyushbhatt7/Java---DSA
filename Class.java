bcc9314355d12e18a12e89e0c0a00508fee020e9
vengersmoon <vengersmon003@gmail.com>
2025-02-05 21:53:46 +0530

    y

--- Class.java
+++ Class.java
@@ -1,5 +1,17 @@
+class Parent {
+
+    void java()
+    {
+        System.out.println("Classe in java");
+    }
+}
+
+
+
+
 public class Class {
     public static void main(String[] args) {
         
+        Parent obj = new Parent();
     }
 }
