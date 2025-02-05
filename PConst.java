9ed3d2f39c4b9b7421bf379024673a2876c6a827
vengersmoon <vengersmon003@gmail.com>
2025-02-05 22:00:15 +0530

    f

--- PConst.java
+++ PConst.java
@@ -1,3 +1,17 @@
+class Constructor
+{
+    Constructor()
+    {
+        System.out.println("This is constructor");
+    }
+}
+
+
+
 public class PConst {
     
+    public static void main(String[] args) {
+        
+        Constructor obj = new Constructor();
+    }
 }
