64a1120c38d80cc6508d00bfcd77f1c8b83395ff
vengersmoon <vengersmon003@gmail.com>
2025-02-06 10:06:46 +0530

    g

--- Abstract.java
+++ Abstract.java
@@ -1,3 +1,24 @@
+abstract class Dog {
+
+    abstract void sound();
+
+    void sleeep()
+    {
+        System.out.println("SLeeping");
+    }
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
 public class Abstract {
     
     public static void main(String[] args) {
