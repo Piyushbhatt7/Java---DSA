67dd31739a3d27f3da8751bbd0b1973b2ba6c48f
vengersmoon <vengersmon003@gmail.com>
2025-02-06 13:00:54 +0530

    f

---
+++ SuperConst.java
@@ -0,0 +1,25 @@
+class Animal {
+
+    Animal()
+    {
+        System.out.println("Animals: ");
+    }
+}
+
+class Dag extends Animal {
+
+    Dog()
+    {
+        super();
+        System.out.println("Barking");
+    }
+}
+
+
+
+public class SuperConst {
+    
+    public static void main(String[] args) {
+        
+    }
+}
