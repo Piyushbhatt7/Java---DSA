e9abfd89bc18e10e72f3fa87d3ec972fdf696f70
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:53:38 +0530

    understanding the logic behind floyd's triangle

--- src/FloydsTriangle.java
+++ src/FloydsTriangle.java
@@ -1,23 +1,21 @@
 
 import java.util.Scanner;
 
-
-
 public class FloydsTriangle {
-    public static void main(String[] args) {
-        
-        int i,j,n;
-        int number = 1;
 
+    public static void main(String[] args) {
+
+        int i, j, n;
+        int number = 1;
 
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
-        n= sc.nextInt();
-       
-        for( i = 1; i<= n; i++){
+        n = sc.nextInt();
 
-            for(j = 1; j<=i; j++){
-                System.out.print(number+ " ");
+        for (i = 1; i <= n; i++) {
+
+            for (j = 1; j <= i; j++) {
+                System.out.print(number + " ");
                 number++;
             }
             System.out.println();
