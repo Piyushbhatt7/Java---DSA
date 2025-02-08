dac9754ee7df87ff055d822d1bbd8ee4808bb57b
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:49:49 +0530

    Checking differnt conditions using(<,><==,if,else)

--- src/Conditional.java
+++ src/Conditional.java
@@ -2,56 +2,44 @@
 import java.util.Scanner;
 
 public class Conditional {
+
     public static void main(String[] args) {
-        
-    //     System.out.print("Enter your age: ");
-    //     Scanner sc = new Scanner(System.in);
-    //     int age = sc.nextInt();
 
-    //     if(age > 18) {
-    //         System.out.println("Adult");
-    //     }
-    //     else{
-    //         System.out.println("Children");
-    //     }
-    
+        //     System.out.print("Enter your age: ");
+        //     Scanner sc = new Scanner(System.in);
+        //     int age = sc.nextInt();
+        //     if(age > 18) {
+        //         System.out.println("Adult");
+        //     }
+        //     else{
+        //         System.out.println("Children");
+        //     }
+        // odd and even number check
+        // System.out.print("Enter any number: ");
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        // if(num % 2 == 0){
+        //     System.out.println("Even number");
+        // }
+        // else {
+        //     System.out.println("Odd number");
+        // }
+        System.out.print("Enter first number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
+        System.out.print("Enter number number: ");
+        Scanner sc1 = new Scanner(System.in);
+        int b = sc1.nextInt();
 
-    // odd and even number check
-    
+        if (a == b) {
 
-    // System.out.print("Enter any number: ");
-    // Scanner sc = new Scanner(System.in);
-    // int num = sc.nextInt();
-    
-    // if(num % 2 == 0){
-    //     System.out.println("Even number");
-    // }
-    // else {
-    //     System.out.println("Odd number");
-    // }
-     
-    System.out.print("Enter first number: ");
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
+            System.out.println("Both number are equal");
+        } else if (a > b) {
+            System.out.println("A is greter then B");
+        } else {
+            System.out.println("A is less then B");
+        }
 
-    System.out.print("Enter number number: ");
-    Scanner sc1 = new Scanner(System.in);
-    int b = sc1.nextInt();
-    
-
-    if(a == b){
-         
-        System.out.println("Both number are equal");
     }
-
-    else if (a > b){
-        System.out.println("A is greter then B");
-    }
-
-    else {
-        System.out.println("A is less then B");
-    }
-    
-  }
 }
