be823c1ed6cb44d1514a2aba1b8c2312b4635f84
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:56:09 +0530

    practicing the method by adding 2 numbers

--- src/FunctionPractice.java
+++ src/FunctionPractice.java
@@ -2,21 +2,20 @@
 import java.util.Scanner;
 
 public class FunctionPractice {
-    
-  public static int sumAdd(int a, int b){
-       
-    int sum = a+b;
-    return sum;
-  }
 
+    public static int sumAdd(int a, int b) {
+
+        int sum = a + b;
+        return sum;
+    }
 
     public static void main(String[] args) {
-          
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-       int sum = sumAdd(a, b);
-       System.out.println(sum);
+        int sum = sumAdd(a, b);
+        System.out.println(sum);
     }
 }
