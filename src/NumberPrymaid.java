0874fe2194ea6822108c691537f1a56e164c8d4f
vengersmoon <vengersmon003@gmail.com>
2025-02-08 23:01:04 +0530

    prymaid in form of numbers

--- src/NumberPrymaid.java
+++ src/NumberPrymaid.java
@@ -2,29 +2,29 @@
 import java.util.Scanner;
 
 public class NumberPrymaid {
-    
+
     public static void main(String[] args) {
-        
-        int i,n,j;
+
+        int i, n, j;
 
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-        for(i=1;i<=n;i++){
+        for (i = 1; i <= n; i++) {
 
-            for(j=1;j<=n-i;j++){
- 
+            for (j = 1; j <= n - i; j++) {
+
                 System.out.print(" ");
 
             }
-            for(j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print(i + " ");
- 
+
             }
             System.out.println();
-           
+
         }
-        
+
     }
 }
