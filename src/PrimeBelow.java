a45c0bd158e8088547358a7bcc2724df2f1e149f
vengersmoon <vengersmon003@gmail.com>
2025-02-08 23:04:57 +0530

    cheking prime number or not

--- src/PrimeBelow.java
+++ src/PrimeBelow.java
@@ -1,28 +1,23 @@
+
 public class PrimeBelow {
-    
+
     public static void main(String[] args) {
 
-        for(int i=2; i<=100;)
-        {
+        for (int i = 2; i <= 100;) {
             int primev = 1;
 
-            for(int loop=2; loop<=i; loop++)
-            {
-                if((i % loop) == 0 && loop!=i)
-                {
+            for (int loop = 2; loop <= i; loop++) {
+                if ((i % loop) == 0 && loop != i) {
                     primev = 0;
                 }
             }
 
-            if(primev != 0) 
-            {
+            if (primev != 0) {
                 System.out.println(i + " is a prime number");
                 i++;
-            }
-           
-            else { 
+            } else {
                 i++;
-            } 
+            }
         }
     }
 }
