a6f77b24f49110c568b45dd57503171f607ed962
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:55:00 +0530

    findian the product of 2 numbers using FunctionMultiply method

--- src/FunctionMultiply.java
+++ src/FunctionMultiply.java
@@ -2,20 +2,20 @@
 import java.util.Scanner;
 
 public class FunctionMultiply {
-    
-  public static int Mlti(int a, int b){
-      
-    int sum = a*b;
-    return sum;   // return a*b;
-  }
+
+    public static int Mlti(int a, int b) {
+
+        int sum = a * b;
+        return sum;   // return a*b;
+    }
 
     public static void main(String[] args) {
-        
+
         System.out.print("Enter 2 numbers: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        
+
         int sum = Mlti(a, b);
         System.out.println("The product of two numbers is = " + sum);
     }
