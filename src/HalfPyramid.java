e57d2c471220927d92be13756dc8609086212e48
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:58:05 +0530

    printing the half prymaid number of rows is equal to number of columns

--- src/HalfPyramid.java
+++ src/HalfPyramid.java
@@ -2,21 +2,21 @@
 import java.util.Scanner;
 
 public class HalfPyramid {
-    
+
     public static void main(String[] args) {
-        
+
         int i, j, n;
         System.out.print("Enter any number: ");
-        Scanner sc = new  Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-        for( i = 1; i <= n; i++){
-             
-            for(j = 1 ; j <= i ; j++){                       // Number of Rows is equal to number of Columns
+        for (i = 1; i <= n; i++) {
+
+            for (j = 1; j <= i; j++) {                       // Number of Rows is equal to number of Columns
                 System.out.print("*");
             }
             System.out.println();
         }
-       
+
     }
 }
