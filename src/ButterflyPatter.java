f1d6f6239114a7dd6e5d4a36de25708826cbf096
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:48:28 +0530

    Printing Butterfly pattern using "*" in 2 half

--- src/ButterflyPatter.java
+++ src/ButterflyPatter.java
@@ -1,47 +1,48 @@
+
 import java.util.Scanner;
 
-public class ButterflyPatter {  
+public class ButterflyPatter {
+
     public static void main(String[] args) {
-        
-        int i,n,j;
+
+        int i, n, j;
 
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         // first half
-        for(i=1;i<=n;i++){
+        for (i = 1; i <= n; i++) {
 
-            for(j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
-            int spaces = 2*(n-i);
+            int spaces = 2 * (n - i);
 
-            for(int k = 1;k<=spaces;k++){
+            for (int k = 1; k <= spaces; k++) {
                 System.out.print(" ");
             }
 
-            for( j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
         // second half
+        for (i = n; i >= 1; i--) {
 
-        for(i=n;i>=1;i--){
-
-            for(j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
-            int spaces = 2*(n-i);
+            int spaces = 2 * (n - i);
 
-            for(int k = 1;k<=spaces;k++){
+            for (int k = 1; k <= spaces; k++) {
                 System.out.print(" ");
             }
 
-            for( j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
