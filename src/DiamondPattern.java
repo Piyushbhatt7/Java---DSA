cd977b3e06f6a70ffad7973e97d1e2731132d699
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:50:51 +0530

    printing diamond pattern in upper half and lower half

--- src/DiamondPattern.java
+++ src/DiamondPattern.java
@@ -2,50 +2,47 @@
 import java.util.Scanner;
 
 public class DiamondPattern {
-    
+
     public static void main(String[] args) {
-        
-        int i,j,k,n;
+
+        int i, j, k, n;
 
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        
 
         // uper half
-        for(i=1;i<=n;i++){
+        for (i = 1; i <= n; i++) {
 
-            
-            for(j=n;j>=i;j--){
+            for (j = n; j >= i; j--) {
                 System.out.print(" ");
             }
-            for(j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
-            for(k=1;k<i;k++){
+            for (k = 1; k < i; k++) {
                 System.out.print("*");
             }
             System.out.println();
 
-        }  
+        }
 
         // lower half
+        for (i = n; i >= 1; i--) {
 
-        for(i=n;i>=1;i--){
-
-            for(j=n;j>=i;j--){
+            for (j = n; j >= i; j--) {
                 System.out.print(" ");
             }
-            for(k=1;k<=i;k++){
+            for (k = 1; k <= i; k++) {
                 System.out.print("*");
             }
 
-            for(k=1;k<i;k++){
+            for (k = 1; k < i; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        
+
     }
 }
