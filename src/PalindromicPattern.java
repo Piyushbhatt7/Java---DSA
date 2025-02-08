e3e3fb249dae48a413f68563aa6879b7bfc77e81
vengersmoon <vengersmon003@gmail.com>
2025-02-08 23:02:49 +0530

    PalindromicPattern means number follows it same number up to down

--- src/PalindromicPattern.java
+++ src/PalindromicPattern.java
@@ -2,26 +2,26 @@
 import java.util.Scanner;
 
 public class PalindromicPattern {
-    
+
     public static void main(String[] args) {
-        
-        int i,j,n;
+
+        int i, j, n;
 
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-        for(i=1;i<=n;i++){
+        for (i = 1; i <= n; i++) {
 
-            for(j=1;j<=n-i;j++){
+            for (j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            for(j=i;j>=1;j--){
+            for (j = i; j >= 1; j--) {
                 System.out.print(j);
             }
-            for(j=2;j<=i;j++){
-                System.out.print(j );
+            for (j = 2; j <= i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
