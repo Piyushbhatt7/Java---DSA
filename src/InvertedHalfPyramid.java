ae44eb6c321b91bc6dc1df0b64a6134ff419723f
vengersmoon <vengersmon003@gmail.com>
2025-02-08 22:58:58 +0530

    Inverted Half Pyramid

--- src/InvertedHalfPyramid.java
+++ src/InvertedHalfPyramid.java
@@ -2,20 +2,20 @@
 import java.util.Scanner;
 
 public class InvertedHalfPyramid {
-    
+
     public static void main(String[] args) {
-        
+
         int i, j, n;
         System.out.print("Enter any number: ");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-       for(i = n; i >= 1 ; i--){
-        
-        for(j = 1; j <= i ; j++){
-            System.out.print("*");
+        for (i = n; i >= 1; i--) {
+
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
-       }
     }
 }
