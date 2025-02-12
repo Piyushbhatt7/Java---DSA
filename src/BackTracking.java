efe09a40c3600423ddc23809ca012c770606d8d0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-12 20:37:05 +0530

    Back Tracking

--- src/BackTracking.java
+++ src/BackTracking.java
@@ -6,6 +6,7 @@ public class BackTracking {
         {
             char currChar = str.charAt(i);
             String newStr = str.substring(0,i) + str.substring(i+1);
+            printPermutation(newStr, perm, idx+1);
         }
     }
     
