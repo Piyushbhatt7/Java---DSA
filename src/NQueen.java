b371986f181ffbaaf0862f9bf313e69a29331e9c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-13 19:18:15 +0530

    Finding all posible cases to place the queen in 4x4 chess board

--- src/NQueen.java
+++ src/NQueen.java
@@ -1,11 +1,13 @@
 public class NQueen {
 
+    public void helper()
+    
     public List<List<String>> solveNQueen(int n)
     {
         List<List<String>> allBoards = new ArrayList<>();
         char[][] board = new board[n][n];
 
-        
+
     }
     
     public static void main(String[] args) {
