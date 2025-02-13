ad09a217b68995692ba4e7c55ea0ce1ad8562443
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-13 19:25:37 +0530

    Finding all posible cases to place the queen in 4x4 chess board

--- src/NQueen.java
+++ src/NQueen.java
@@ -12,7 +12,7 @@ public class NQueen {
             {
                 board[row][col] = 'Q';
                 helper(board, allBoards, col + 1);
-                board[row][col] = '.'
+                board[row][col] = '.';
             }
         }
     }
