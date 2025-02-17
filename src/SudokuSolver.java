e6a97b678a470b6bb4b92184568fb989d69f40ae
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-17 11:56:11 +0530

    Sudoku Solver

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -2,12 +2,12 @@ public class SudokuSolver {
 
     public boolean helper(char[][] board, int row, int col)
     {
-        
+
     }
     
     public void solveSudoku(char[][] board)
     {
-
+       helper(board, 0, 0)
     }
     
     public static void main(String[] args) {
