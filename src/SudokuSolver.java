0a75365d7f949f388e8a7f42be2621dc87461b2e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-17 12:20:18 +0530

    SudokuSolver

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -90,6 +90,15 @@ public class SudokuSolver {
     {
        helper(board, 0, 0);
     }
+
+    public void printBoard(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     
     public static void main(String[] args) {
         
