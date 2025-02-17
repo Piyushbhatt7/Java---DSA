e710825773f617103d47a315a2a5837576e96e55
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-17 23:33:31 +0530

    h

--- src/FileExample.java
+++ src/FileExample.java
@@ -22,7 +22,7 @@ public class FileExample {
             System.out.println("File created: " + file.canRead());
         } 
         catch (IOException e) {
-            System.out.println("An error occurred.");  //hkyyyferttye6
+            System.out.println("An error occurred.");  //hkyyyferttye6e
             e.printStackTrace();
         }
     }
