579a5bdf61fd18359adf8c1051bf9f4de7c2812f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-17 12:09:24 +0530

    Sudoku Solver

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,5 +1,10 @@
 public class SudokuSolver {
 
+    public boolean isSafe(char[][] board, int row, int col, int number)
+    {
+        
+    }
+
     public boolean helper(char[][] board, int row, int col)
     {
 
