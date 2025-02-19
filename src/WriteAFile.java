7f2b07706418720d95dcab95aa59509939efeb9e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-19 21:58:39 +0530

    Createing a New File

--- src/WriteAFile.java
+++ src/WriteAFile.java
@@ -1,6 +1,7 @@
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class WriteAFile {
     
@@ -11,8 +12,11 @@ public class WriteAFile {
         try {
             
             FileWriter myFileWriter = new FileWriter("Newfile.txt");
+            myFileWriter.write("new file created a writing on it");
         } 
-        catch (Exception e) {
+        catch (IOException e) {
+            System.out.println("Failed to create a file");
+            e.printStackTrace();
         }
     }
 }
