f8fdc9cd5258bdde88fe38d8b1a7eb065345d5c1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-19 21:54:55 +0530

    Createing a New File

--- src/WriteAFile.java
+++ src/WriteAFile.java
@@ -1,3 +1,15 @@
+
+import java.io.File;
+
 public class WriteAFile {
     
+    public static void main(String[] args) {
+        
+        File myFile = new File("Newfile.txt");
+
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
 }
