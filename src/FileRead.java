d6be5b42e6ca48a840c32f4169fc0c0bd459100e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-19 22:43:25 +0530

    reading a existing file

--- src/FileRead.java
+++ src/FileRead.java
@@ -13,6 +13,11 @@ public class FileRead {
         try {
 
             Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine())
+            {
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
             
         } catch (Exception e) {
 
