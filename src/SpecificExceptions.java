f84d24d9ee6c750356c358261efef1b91df2c729
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-19 23:17:14 +0530

    Handling Specific Exceptions

--- src/SpecificExceptions.java
+++ src/SpecificExceptions.java
@@ -3,6 +3,8 @@ public class SpecificExceptions {
     public static void main(String[] args) {
         
         int marks[] = new int [3];
-
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 12;
     }
 }
