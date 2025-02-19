00c09ee8a18e62c79fc05bc5277d8e9fcacb0d9c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-19 10:21:16 +0530

    File handling in java

--- src/Files.java
+++ src/Files.java
@@ -1,22 +1,26 @@
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
+
+
 
 public class Files {
     
     public static void main(String[] args) {
         
         // code to create a new file
-        File myFile = new File("Files.txt");
+        // File myFile = new File("Files.txt");
 
-        try {
-            myFile.createNewFile();
-        } 
-        catch (IOException e) {
-            System.out.println("Unable to create this file");
-            e.printStackTrace();
-        }
+        // try {
+        //     myFile.createNewFile();
+        // } 
+        // catch (IOException e) {
+        //     System.out.println("Unable to create this file");
+        //     e.printStackTrace();
+        // }
 
         // code to write to a file
+
+        FileWriter myFileWriter = new FileWriter("chwfile.txt")
     }
 }
