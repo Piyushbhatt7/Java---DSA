9be60d72dfde3d56e9037f16942137e63f7b54bf
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-20 21:18:54 +0530

    k

--- src/NestesdTryCatch.java
+++ src/NestesdTryCatch.java
@@ -32,7 +32,7 @@ public class NestesdTryCatch {
             }
         } 
         catch (Exception e) {
-    // uuuuiyuuuuyyerrkryeurytrtyr
+    // uuuuiyuuuuyyerrkryeurytrtyry
             System.out.println("Exception in level 2");
         } 
 
