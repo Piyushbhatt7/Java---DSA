49a71d7c14035dad7e761be552407746c7d95626
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-20 12:01:05 +0530

    Nestesd Try Catch

--- src/NestesdTryCatch.java
+++ src/NestesdTryCatch.java
@@ -1,10 +1,21 @@
 public class NestesdTryCatch {
     
     public static void main(String[] args) {
+
+        int marks[] = new int [3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 12;
         
         try {
             
             System.out.println("WElcome to nested try catch");
+
+            try {
+                
+            } 
+            catch (Exception e) {
+            }
         } 
         catch (Exception e) {
 
