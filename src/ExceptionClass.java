cbd23c93d7a91fce4fa2110ae8c3a275b802ce2e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 21:57:01 +0530

    Exception Class

--- src/ExceptionClass.java
+++ src/ExceptionClass.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 
 public class ExceptionClass  extends Exception{
 
+
+    public ExceptionClass(String message)
+    {
+        super(message);
+    }
     @Override
     public String toString()
     {
-        return super.toString() + "- I am toString()";
+        return super.toString() + " - I am toString()";
     }
 
     @Override
     public String getMessage()
     {
-        return super.getMessage() + "- I am getMessage()";
+        return super.getMessage() + " - I am getMessage()";
     }
     
     public static void main(String[] args) {
@@ -24,11 +29,12 @@ public class ExceptionClass  extends Exception{
         {
 
            try {
-            throw new ExceptionClass();
+            throw new ExceptionClass("NUmber is greater then 9");
            } 
            catch (Exception e) {
               
             System.out.println(e.getMessage());
+            System.out.println(e.toString());
            }
         }
     }
