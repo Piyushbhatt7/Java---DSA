ef8ae81a4752fccc4f61af2abf4f8ec71e59a35d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 11:01:17 +0530

    Abstract Class

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -8,6 +8,13 @@ public class AbstractClass {
         // abstract method is declared without an implementation
 
         // abstract void moveTo(double x, double y);
+
+        // abstract class
+        // if a class includes abstract method, then the class itself must be declared abstract, as in:
+
+        // public abstract class PhoneModel {
+        //   abstract void switchoff();
+        // }
     }
    
 }
