7715db03cd90bbfe03087bf597a2aab01a03ef06
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 10:57:54 +0530

    Abstract Class

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -3,6 +3,9 @@ public class AbstractClass {
     public static void main(String args[])
     {
         // existing in thought or as an idea but not having a physical or concrete existence.
+         
+
+        // abstract method is declared without an implementation
         
     }
    
