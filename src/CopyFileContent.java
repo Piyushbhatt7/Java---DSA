a3841f7054a3633de853f93aa1dd8433a6b0d703
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 11:45:13 +0530

    Copy File Content

--- src/CopyFileContent.java
+++ src/CopyFileContent.java
@@ -2,5 +2,9 @@ public class CopyFileContent{
 
     public static void main(String[] args) {
         
+        String source = "mysource.txt";
+        String destination  = "mydestination.txt";
+
+        
     }
 }
