0739f52846a5b1e20347f15eb32fe46abb4d6238
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 21:44:29 +0530

    Exception Class

--- src/ExceptionClass.java
+++ src/ExceptionClass.java
@@ -1,4 +1,9 @@
 public class ExceptionClass  extends Exception{
+
+    public String toString()
+    {
+        return super.toString();
+    }
     
     public static void main(String[] args) {
         
