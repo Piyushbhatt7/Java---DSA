d2154ce1aad9a1dce5a3a7b2b54ee15e4a874321
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 10:53:26 +0530

    Abstract Class

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -3,6 +3,7 @@ public class AbstractClass {
     public static void main(String args[])
     {
         // existing in thought or as an idea but not having a physical or concrete existence.
+        
     }
    
 }
