c39de27e49ebde8aba261dc09ed84b174925cc3b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-21 11:48:39 +0530

    Copy File Content

--- src/CopyFileContent.java
+++ src/CopyFileContent.java
@@ -1,3 +1,7 @@
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class CopyFileContent{
 
     public static void main(String[] args) {
@@ -5,6 +9,18 @@ public class CopyFileContent{
         String source = "mysource.txt";
         String destination  = "mydestination.txt";
 
-        
+        try (FileReader fr = new FileReader(source); 
+            FileWriter fw = new FileWriter(destination)
+        )
+        {
+            int chararacter;
+
+            while((chararacter = fr.read()) != -1)
+            {
+                fw.write(chararacter);
+            }
+
+            System.out.println("File content copied successfull");
+        }
     }
 }
