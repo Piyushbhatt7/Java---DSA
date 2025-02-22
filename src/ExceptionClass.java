92ef22eed630709b220b693b120011ff32dd5003
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-22 22:17:18 +0530

    y

--- src/ExceptionClass.java
+++ src/ExceptionClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ExceptionClass  extends Exception{
 
  
-    public ExceptionClass(String message)    
+    public ExceptionClass(String message)     
     {
         super(message);
     }
