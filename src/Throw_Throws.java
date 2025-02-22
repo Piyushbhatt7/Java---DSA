a312e0ac143092a199d85ab5190d8789434d988c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-22 11:31:16 +0530

    Difference between Throw and Throws in java

--- src/Throw_Throws.java
+++ src/Throw_Throws.java
@@ -1,6 +1,6 @@
 public class Throw_Throws {
     
-    static int divide(int a, int b) throws ArithmeticException
+    static int divide(int a, int b) throws ArithmeticException 
     {
         // made by harry
         int result = a/b;
@@ -9,6 +9,12 @@ public class Throw_Throws {
     public static void main(String[] args) {
         
         // porter - uses divide function created by harry
+        try {
+            
+        } 
+        catch (Exception e) {
+            
+        }
         int c = divide(6,0);
         System.out.println(c);
     }
