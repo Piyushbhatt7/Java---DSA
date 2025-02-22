42b6dce137ac515e17242e9854eb4903656cf2ba
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-22 11:10:15 +0530

    Difference between Throw and Throws in java

--- src/Throw_Throws.java
+++ src/Throw_Throws.java
@@ -2,12 +2,12 @@ public class Throw_Throws {
     
     int divide(int a, int b)
     {
-        // made by result
+        // made by harry
         int result = a/b;
         return result;
     }
     public static void main(String[] args) {
         
-
+        // porter - uses divide function created by harry
     }
 }
