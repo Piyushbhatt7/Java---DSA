c70b4ff563c7b2d99c24f9a1c254b584f0e2b14b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-23 20:30:53 +0530

    Finally Block

--- src/Finally_Block.java
+++ src/Finally_Block.java
@@ -1,6 +1,6 @@
 public class Finally_Block {
 
-    public static void greet()
+    public static int greet()
     {
          
         try {
@@ -8,7 +8,6 @@ public class Finally_Block {
             int a = 50;
             int b = 0;
             int c = a / b;
-            return c;
         }
 
         catch(Exception e)
