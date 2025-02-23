3ff34ba7bc91b318c1796e9565a85276f38ca80a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-23 19:41:10 +0530

    Finally Block

--- src/Finally_Block.java
+++ src/Finally_Block.java
@@ -2,5 +2,12 @@ public class Finally_Block {
     
     public static void main(String[] args) {
         
+        try {
+
+            int a = 5;
+            int b = 0;
+        }
+
+        catch
     }
 }
