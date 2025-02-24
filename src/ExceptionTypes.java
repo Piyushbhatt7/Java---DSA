1b08e581b4c034b645a18624b4c3ece3501e3654
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 13:48:59 +0530

    Exception Types in java

--- src/ExceptionTypes.java
+++ src/ExceptionTypes.java
@@ -1,11 +1,15 @@
+
+import java.util.Scanner;
+
 public class ExceptionTypes {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
         int remainder;
         int denominator;
         int result;
 
-        
+
     }
 }
