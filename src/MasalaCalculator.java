77a0b71193e6caaaf0420903173b4fea8517e8dc
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 11:58:36 +0530

    Custom Calculator | Java Practice Question

--- src/MasalaCalculator.java
+++ src/MasalaCalculator.java
@@ -2,6 +2,15 @@ public class MasalaCalculator {
     
     public static void main(String[] args) {
         
+        // Problem - You have to create a custom calculator with 
+        //following operations :-
+       
+        // 1. + - Addition
+        // 2. - - Subtraction
+        // 3. * - Multiplication
+        // 4. / - Division
+        // 5. % - Modulor
         
+         // which throws the following exceptions :-
     }
 }
