59807b5de13c361c74b2bffa9059c099d2af3cda
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 13:53:59 +0530

    Exception Types in java

--- src/ExceptionTypes.java
+++ src/ExceptionTypes.java
@@ -7,28 +7,33 @@ public class ExceptionTypes {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number: ");
+        System.out.print("Enter first number: ");
         int remainder = sc.nextInt();
-        System.out.println("Enter second number: ");
+        System.out.print("Enter second number: ");
         int denominator = sc.nextInt();
         int result;
 
         try{
 
             result = remainder / denominator;
-            System.out.println("Result: " + result);
+            System.out.print("Result: " + result);
         }
 
         catch(ArithmeticException e)
         {
             System.out.println("Cannot divide by Zero!");
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
         catch(InputMismatchException e)
         {
             System.out.println("Wrong input giver!");
             System.out.println(e.getMessage());
+        }
+
+        finally {
+            System.out.println("Programm ended!");
         }
 
 
