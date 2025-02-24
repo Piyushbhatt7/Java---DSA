95cbb0213a7864bc8dcb92e238091674ac96c784
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 11:41:08 +0530

    Exception handling Practice Sets

--- src/PracticeSet.java
+++ src/PracticeSet.java
@@ -3,23 +3,32 @@ public class PracticeSet {
     public static void main(String[] args) {
         
         // problem 1
+
         // Syntax Error - int a = 7
         // int age = 78;
         // Logical Error - int year_born = 2000-78;
-        System.out.println(6/0);
+        //System.out.println(6/0);
 
         // problem 2
 
-        try {
-            int a = 66/9;
+        // try {
+        //     int a = 66/0;
 
-        } 
-        catch (IllegalArgumentException e) {
-             System.out.println("haha");
-        }
-        catch(ArithmeticException e)
+        // } 
+        // catch (IllegalArgumentException e) {
+        //      System.out.println("hehe");
+        // }
+        // catch(ArithmeticException e)
+        // {
+        //     System.out.println("haha");
+        // }
+
+        // problem 3
+
+        boolean flag = true;
+        while(flag)
         {
-            System.out.println("hhehe");
+            
         }
 
     }
