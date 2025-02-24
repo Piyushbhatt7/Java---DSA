9447a820f4477882ac8f3a2163ff647b63c8e379
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 23:19:50 +0530

    Packages in Java

--- src/PackagesinJava.java
+++ src/PackagesinJava.java
@@ -1,8 +1,12 @@
-package piyush;
+
+import java.util.Scanner;
+
+// package piyush;
 public class PackagesinJava {
     
     public static void main(String[] args) {
         
-        System.out.println("This is a package in java");
+       // System.out.println("This is a package in java");
+       Scanner sc = new Scanner(System.in);
     }
 }
