269bfe4d6ea2d8f7837aed90718777d4c51f46d1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 11:35:51 +0530

    Exception handling Practice Sets

--- src/PracticeSet.java
+++ src/PracticeSet.java
@@ -5,8 +5,8 @@ public class PracticeSet {
         // problem 1
         // Syntax Error - int a = 7
         // int age = 78;
-        // int year_born = 2000-78;
-        
+        // Logical Error - int year_born = 2000-78;
+
 
     }
 }
