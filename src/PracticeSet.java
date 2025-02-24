9816ce1ec83dc89c322d163d3b9a798af1cdfadf
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-24 11:37:14 +0530

    Exception handling Practice Sets

--- src/PracticeSet.java
+++ src/PracticeSet.java
@@ -8,6 +8,15 @@ public class PracticeSet {
         // Logical Error - int year_born = 2000-78;
         System.out.println(6/0);
 
+        // problem 2
+
+        try {
+            
+
+        } 
+        catch (Exception e) {
+            
+        }
 
     }
 }
