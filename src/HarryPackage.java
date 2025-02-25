67cf3d248b3e655017e30248d0663a12f9c519ce
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-25 10:05:23 +0530

    making custom package

--- src/HarryPackage.java
+++ src/HarryPackage.java
@@ -1,7 +1,10 @@
 //package piyush.gym;
+import piyush.lean;
 public class HarryPackage {
     
     public static void main(String[] args) {
         
+        PackagesinJava obj = new PackagesinJava();
+
     }
 }
