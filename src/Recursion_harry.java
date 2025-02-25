c31f47239d077767959020dd9da4cd39588ff469
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-25 10:30:29 +0530

    Recursion in java

--- src/Recursion_harry.java
+++ src/Recursion_harry.java
@@ -1,5 +1,13 @@
 public class Recursion_harry {
     
+   static void multiplication(int n)
+   {
+    for(int i=1; i<=10; i++)
+    {
+        System.out.println();
+    }
+   }
+
     public static void main(String[] args) {
         
     }
