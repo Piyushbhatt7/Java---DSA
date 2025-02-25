1a66fcdf2d46ce50bd01b677cd89290a71a2a930
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-25 10:22:34 +0530

    Recursion in java

--- src/recursion.java
+++ src/recursion.java
@@ -1,19 +1,19 @@
 public class recursion {
     
+    static int fact(int n)
+    {
+      if(n==0 || n==1)
+      {
+        return 1;
+      }
+      else{
+        return n*fact(n-1);
+      }
+
+      
+    }
     public static void main(String[] args) {
         
 
-        static int fact(int n)
-        {
-          if(n==0 || n==1)
-          {
-            return 1;
-          }
-          else{
-            return n*fact(n-1);
-          }
-
-          return 0;
-        }
     }
 }
