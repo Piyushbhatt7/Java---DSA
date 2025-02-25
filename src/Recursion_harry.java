f314b567642e24b58d49e80333c15d9b114b7f69
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-25 10:33:01 +0530

    Recursion in java

--- src/Recursion_harry.java
+++ src/Recursion_harry.java
@@ -4,7 +4,7 @@ public class Recursion_harry {
    {
     for(int i=1; i<=10; i++)
     {
-        System.out.format("%d X %d = %d\n", n, i, n*i);
+        System.out.format("%d x %d = %d\n", n, i, n*i);
     }
    }
 
