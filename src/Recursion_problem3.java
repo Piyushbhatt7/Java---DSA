903df2e250b051f56e670f44f79e4358065892af
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-25 23:14:49 +0530

    Recursion_problem3

--- src/Recursion_problem3.java
+++ src/Recursion_problem3.java
@@ -1,5 +1,17 @@
 public class Recursion_problem3 {
     
+    static int sumRect(int n)
+
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+
+        else {
+            return n + sumRect(n-1);
+        }
+    }
     public static void main(String[] args) {
         
     }
