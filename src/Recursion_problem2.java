3e62e8aec9b115fb3a8321e1de33f60684f64706
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-25 23:09:17 +0530

    g

--- src/Recursion_problem2.java
+++ src/Recursion_problem2.java
@@ -4,7 +4,7 @@ public class Recursion_problem2 {
    { 
       for(int i=0; i<n; i++)
       {
-         for(int j=0; j<i+1; j++)
+         for(int j=0; j<i+1; j++) 
          {
             System.out.print("*"); 
          }
