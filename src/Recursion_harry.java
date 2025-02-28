1f655e7f651ac001385d16ddb855c4bc2b1ebcf8
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-28 21:32:18 +0530

    Recursion Practice

--- src/Recursion_harry.java
+++ src/Recursion_harry.java
@@ -2,7 +2,7 @@ public class Recursion_harry {
     
    static void multiplication(int n)
    {
-    for(int i=1; i<=10; i++)
+    for(int i=1; i<=10; i++) 
     {
         System.out.format("%d x %d = %d\n", n, i, n*i);
     }
