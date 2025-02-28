68f76d68ea2b10570eed7ef36414e2607ca805bc
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-28 21:32:29 +0530

    Recursion Practice

--- src/Recursion_harry.java
+++ src/Recursion_harry.java
@@ -5,7 +5,7 @@ public class Recursion_harry {
     for(int i=1; i<=10; i++) 
     {
         System.out.format("%d x %d = %d\n", n, i, n*i);
-    }
+    } 
    }
 
     public static void main(String[] args) {
