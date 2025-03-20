3ddcbcb946ae5ab412cb00d46a834a79301d4919
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-20 11:16:54 +0530

    packages in java

--- src/MyFile.java
+++
@@ -1,9 +0,0 @@
-package piyush;
-
-public class MyFile {
-    
-    public static void main(String[] args) {
-        System.out.println("package piyush ");
-    }
-}
- 
---
+++ src/Piyush.java
@@ -0,0 +1,6 @@
+public class Piyush {
+    
+    public static void main(String[] args) {
+        System.out.println("Package -> PIYUSH");
+    }
+}
